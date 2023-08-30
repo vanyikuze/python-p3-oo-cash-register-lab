3cf557db850800054a3c658cf75cb77da153e640
Vany <vany.ikuze@student.moringaschool.com>
2023-08-30 14:30:20 +0300

    Donr

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([title] * quantity)
+
+    def apply_discount(self):
+        if self.discount > 0:
+            self.total *= (1 - self.discount / 100)
+
+    def void_last_transaction(self):
+        if self.items:
+            last_item_price = self.total - (self.total - self.items[-1])
+            self.total -= last_item_price
+            self.items.pop()
+
